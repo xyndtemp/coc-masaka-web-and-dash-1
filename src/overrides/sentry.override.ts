6669eca7346effd80085c20886957a3bc25569ae
Prince Shammah <shammahprinz@gmail.com>
2024-10-05 21:23:24 +0100

    fix: fix sentry

--- src/overrides/sentry.override.ts
+++ src/overrides/sentry.override.ts
@@ -1,75 +1,19 @@
-import { getCurrentScope } from '@sentry/browser';
-import { captureConsoleIntegration } from '@sentry/integrations';
-import type { BrowserOptions } from '@sentry/react';
-import {
-    init,
-    reactRouterV6BrowserTracingIntegration,
-    replayIntegration
-} from '@sentry/react';
-import type { Integration } from '@sentry/types';
-import { useEffect } from 'react';
-import {
-    createRoutesFromChildren,
-    matchRoutes,
-    useLocation,
-    useNavigationType
-} from 'react-router-dom';
+import * as Sentry from "@sentry/react";
 import defaultConfig from '../config/default.json';
 
 const RELEASE_VERSION = import.meta.env.VITE_RELEASE_VERSION || 'unknown';
 const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENT || 'development';
 
-
-const SentryInit = () => {
-    const options: BrowserOptions = {
-        dsn: defaultConfig.sentry.dsn,
-        tracesSampleRate: 1.0,
-        replaysSessionSampleRate: 0.1,
-        replaysOnErrorSampleRate: 0.1,
-        release: RELEASE_VERSION,
-        environment: ENVIRONMENT,
-        enabled: Boolean(defaultConfig.env !== 'local') // Prevents new bugs while dev is typing
-    };
-
-
-        const integrations: Integration[] = [
-            reactRouterV6BrowserTracingIntegration({
-                useEffect,
-                useLocation,
-                useNavigationType,
-                createRoutesFromChildren,
-                matchRoutes
-                // stripBasename
-            }),
-            replayIntegration(),
-            captureConsoleIntegration({
-                levels: ['error']
-            })
-        ];
-
-        options.tracePropagationTargets = [config.host || 'localhost'];
-
-        if (defaultConfig.env !== 'local') {
-            integrations.push(
-                captureConsoleIntegration({
-                    levels: ['error', 'warn']
-                })
-            );
-        }
-
-        options.integrations = integrations;
-
-        init(options);
-
-        getCurrentScope().setTags({
-            // eslint-disable-next-line camelcase
-            // session_id: getSessionId(),
-            // eslint-disable-next-line camelcase
-            // browser_id: getBrowserId()
-        });
-    
-};
-
-SentryInit();
-
-export * from '@sentry/react';
+Sentry.init({
+    dsn: defaultConfig.sentry.dsn,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+    ],
+    release: RELEASE_VERSION,
+    environment: ENVIRONMENT,
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
